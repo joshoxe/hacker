0856e0ee32268503648d06cc57dfebddc805e899
josh <jhowdy123@gmail.com>
2017-10-04 18:28:02 +0100

    Add method to load random words into a list
    Will load random words with a length based on the a chosen difficulty
    Uses popular.txt from github user dolph, based on enable1.txt but all uncommon words removed

---
+++ main.py
@@ -0,0 +1,14 @@
+from difficulty import Difficulty
+import random
+
+def load_words(difficulty_level):
+    """Loads random words from a dictionary with length equal to difficulty"""
+    dictionary = open("popular.txt").readlines()
+    words = []
+    while len(words) < 10:
+        word = random.choice(dictionary)
+        word = word.rstrip('\r\n')
+        if len(word) == difficulty_level:
+            if word not in words:
+                words.append(word)
+    return words
