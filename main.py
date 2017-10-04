9513f09d3d52c6549aeb6b57c1724ddcdeb13d04
josh <jhowdy123@gmail.com>
2017-10-04 18:54:25 +0100

    Add method to count correct letter positions

--- main.py
+++ main.py
@@ -12,3 +12,11 @@ def load_words(difficulty_level):
             if word not in words:
                 words.append(word)
     return words
+
+def count_positions(guess, password):
+    """Counts how many letter positions are correct for a guess"""
+    count = 0
+    for a, b in zip(guess, password):
+        if a == b:
+            count = count + 1
+    return count
